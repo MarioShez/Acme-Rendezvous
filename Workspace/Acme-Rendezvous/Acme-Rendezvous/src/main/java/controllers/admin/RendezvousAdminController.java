87a0d3af1e7812f1978d0353d5581c60a90f0102
Juan Rodríguez Dueñas <juaniito.rodriiguez92@gmail.com>
2018-03-08 00:25:07 +0100

    Controllers: solventado los errores de review B

--- Workspace/Acme-Rendezvous/Acme-Rendezvous/src/main/java/controllers/admin/RendezvousAdminController.java
+++ Workspace/Acme-Rendezvous/Acme-Rendezvous/src/main/java/controllers/admin/RendezvousAdminController.java
@@ -1,11 +1,16 @@
+
 package controllers.admin;
+
+import java.util.Collection;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.Assert;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import services.AdminService;
 import services.RendezvousService;
 import controllers.AbstractController;
 import domain.Rendezvous;
@@ -17,7 +22,11 @@ public class RendezvousAdminController extends AbstractController {
 	// Services ------------------------------------------------------
 
 	@Autowired
-	private RendezvousService rendezvousService;
+	private RendezvousService	rendezvousService;
+
+	@Autowired
+	private AdminService		adminService;
+
 
 	// Constructors --------------------------------------------------
 
@@ -25,15 +34,34 @@ public class RendezvousAdminController extends AbstractController {
 		super();
 	}
 
+	// Listing	------------------------------------------------------
+
+	@RequestMapping(value = "/list", method = RequestMethod.GET)
+	public ModelAndView list() {
+
+		Assert.notNull(this.adminService.findByPrincipal());
+
+		final Collection<Rendezvous> rendezvouses = this.rendezvousService.findAll();
+
+		final ModelAndView result = new ModelAndView("rendezvous/list");
+		result.addObject("rendezvouses", rendezvouses);
+		result.addObject("rendezvousSource", null);
+		result.addObject("requestURI", "rendezvous/admin/list.do");
+
+		return result;
+	}
+
 	// Delete	------------------------------------------------------
-	
+
 	@RequestMapping(value = "/delete", method = RequestMethod.GET)
-	public ModelAndView delete(int rendezvousId){
-		
-		Rendezvous rendezvous = rendezvousService.findOne(rendezvousId);
-		
-		rendezvousService.delete(rendezvous);
-		
+	public ModelAndView delete(final int rendezvousId) {
+
+		Assert.notNull(this.adminService.findByPrincipal());
+
+		final Rendezvous rendezvous = this.rendezvousService.findOne(rendezvousId);
+
+		this.rendezvousService.delete(rendezvous);
+
 		return new ModelAndView("redirect:/welcome/index.do");
 	}
 
