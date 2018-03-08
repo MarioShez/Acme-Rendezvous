54b60d7f54c093cc639cb58ee9b63fc12c07a5e1
Mario <codec993@gmail.com>
2018-03-08 18:56:52 +0100

    Manager

--- Workspace 2.0/Acme-Rendezvous/Acme-Rendezvous/src/main/java/domain/Manager.java
+++ Workspace 2.0/Acme-Rendezvous/Acme-Rendezvous/src/main/java/domain/Manager.java
@@ -1,14 +1,9 @@
 package domain;
 
-import java.util.Collection;
-import java.util.Date;
 
 import javax.persistence.Access;
 import javax.persistence.AccessType;
 import javax.persistence.Entity;
-import javax.persistence.OneToMany;
-import javax.validation.Valid;
-import javax.validation.constraints.NotNull;
 
 import org.hibernate.validator.constraints.NotBlank;
 import org.hibernate.validator.constraints.SafeHtml;
@@ -27,10 +22,6 @@ public class Manager extends Actor {
 
 	private String vat;
 
-	// Relationships
-
-//	private Collection<Service> service;
-	
 	
 	@NotBlank
 	@SafeHtml
@@ -42,16 +33,5 @@ public class Manager extends Actor {
 		this.vat = vat;
 	}
 	
-	
-//	@Valid
-//	@NotNull
-//	@OneToMany(mappedBy = "manager")
-//	public Collection<Service> getService() {
-//		return service;
-//	}
-//
-//	public void setService(Collection<Service> service) {
-//		this.service = service;
-//	}
 
 }
