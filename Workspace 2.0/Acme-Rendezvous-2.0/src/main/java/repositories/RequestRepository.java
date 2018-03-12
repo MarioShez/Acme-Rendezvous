c2408a2089656bbbf31dc8db865b386dea322fe3
Juan Rodríguez Dueñas <juaroddue@alum.us.es>
2018-03-12 19:40:46 +0100

    Repositories: Añadido repositorios nivel C.

--- Workspace 2.0/Acme-Rendezvous-2.0/src/main/java/repositories/RequestRepository.java
+++ Workspace 2.0/Acme-Rendezvous-2.0/src/main/java/repositories/RequestRepository.java
@@ -1,11 +1,12 @@
+
 package repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import domain.Services;
+import domain.Request;
 
 @Repository
-public interface ServicesRepository extends JpaRepository<Services, Integer>{
+public interface RequestRepository extends JpaRepository<Request, Integer> {
 
 }
---
+++ Workspace 2.0/Acme-Rendezvous-2.0/src/main/java/repositories/ServiceRepository.java
@@ -0,0 +1,18 @@
+
+package repositories;
+
+import java.util.Collection;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import domain.Service;
+
+@Repository
+public interface ServiceRepository extends JpaRepository<Service, Integer> {
+
+	@Query("select s, s.requests.size from Service s order by s.requests.size desc")
+	Collection<Service> bestSellingServices();
+
+}
