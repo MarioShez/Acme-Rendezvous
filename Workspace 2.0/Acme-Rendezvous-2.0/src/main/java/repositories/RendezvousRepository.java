2fb4ff6bdca4f2f1197457828a36a725e8f1a005
marmargal <inma.martin.galvan@hotmail.com>
2018-03-21 20:19:36 +0100

    Solución query

--- Workspace 2.0/Acme-Rendezvous-2.0/src/main/java/repositories/RendezvousRepository.java
+++ Workspace 2.0/Acme-Rendezvous-2.0/src/main/java/repositories/RendezvousRepository.java
@@ -38,7 +38,7 @@ public interface RendezvousRepository extends JpaRepository<Rendezvous, Integer>
 	@Query("select r from Rendezvous r join r.linkedRendezvouses l where l.id=?1")
 	Collection<Rendezvous> findParentRendezvouses(int rendezvousId);
 
-	@Query("select r.rendezvous from Request r join r.service.categories c where c.id=?1")
+	@Query("select r.rendezvous from Request r join r.service.category c where c.id=?1")
 	Collection<Rendezvous> rendezvousGroupedByCategory(int categoryId);
 	
 
