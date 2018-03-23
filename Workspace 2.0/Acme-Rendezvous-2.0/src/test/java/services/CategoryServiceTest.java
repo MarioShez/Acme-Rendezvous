550672fc7f466b6b3ae3a30e006cf54e1168da54
marmargal <inma.martin.galvan@hotmail.com>
2018-03-23 15:44:31 +0100

    Test Category y cambios en Populate

--- Workspace 2.0/Acme-Rendezvous-2.0/src/test/java/services/CategoryServiceTest.java
+++ Workspace 2.0/Acme-Rendezvous-2.0/src/test/java/services/CategoryServiceTest.java
@@ -21,10 +21,13 @@ import utilities.AbstractTest;
 public class CategoryServiceTest extends AbstractTest{
 	
 	// The SUT -------------------------------------
+	
 	@Autowired
 	private CategoryService categoryService;
 	
+	
 	// Create and Save Test -------------------------
+	
 	@Test
 	public void driverCreateAndSave(){
 		Object testingData[][]={
@@ -66,9 +69,7 @@ public class CategoryServiceTest extends AbstractTest{
 			category.setName(name);
 			category.setDescription(description);
 			parent = this.categoryService.findOne(parentId);
-			if(parent!=null){
-				category.setCategoryParent(parent);
-			}
+			category.setCategoryParent(parent);
 			category = this.categoryService.save(category);
 			this.categoryService.flush();
 			unauthenticate();
@@ -78,5 +79,108 @@ public class CategoryServiceTest extends AbstractTest{
 		checkExceptions(expected, caught);
 		
 	}
+	
+	
+	
+	// Edit and Save Test -------------------------
+	
+	@Test
+	public void driverEditTest(){
+		Object testingData[][] = {
+				// positive test
+				{
+					"admin", "category2", "new name", "new description", "category1", null
+				},
+				// negative test: description blank
+				{
+					"admin", "category2", "new name", " ", "category1", javax.validation.ConstraintViolationException.class
+				},
+				// negative test: user1
+				{
+					"user1", "category2", "new name", "new description", "category1", IllegalArgumentException.class
+				},
+		};
+		for (int i=0; i<testingData.length; i++){
+			templateEdit((String)testingData[i][0],
+					super.getEntityId((String)testingData[i][1]),
+					(String)testingData[i][2],
+					(String)testingData[i][3],
+					super.getEntityId((String)testingData[i][4]),
+					(Class<?>) testingData[i][5]);
+		}
+	}
+	
+	protected void templateEdit(String username, int categoryId, String name,
+			String description, int parentId, Class<?> expected) {
+		
+		Category category;
+		Category parent;
+		Class<?> caught;
+		
+		caught = null;
+		
+		try{
+			authenticate(username);
+			category = this.categoryService.findOne(categoryId);
+			category.setName(name);
+			category.setDescription(description);
+			parent = this.categoryService.findOne(parentId);
+			category.setCategoryParent(parent);
+			category = this.categoryService.save(category);
+			this.categoryService.flush();
+			unauthenticate();
+		}catch (Throwable oops) {
+			caught = oops.getClass();
+		}
+		checkExceptions(expected, caught);
+		
+	}
+	
+	
+	// Delete Test -------------------------
+	
+		@Test
+		public void driverDeleteTest(){
+			Object testingData[][] = {
+					// positive test
+					{
+						"admin", "category5", null
+					},
+					// negative test: category.getServices not empty
+					{
+						"admin", "category2", IllegalArgumentException.class
+					},
+					// negative test: user1
+					{
+						"user1", "category5", IllegalArgumentException.class
+					},
+					
+			};
+			for (int i=0; i<testingData.length; i++){
+				templateDelete((String)testingData[i][0],
+						super.getEntityId((String)testingData[i][1]),
+						(Class<?>) testingData[i][2]);
+			}
+		}
+		
+		protected void templateDelete(String username, int categoryId, Class<?> expected) {
+			
+			Category category;
+			Class<?> caught;
+			
+			caught = null;
+			
+			try{
+				authenticate(username);
+				category = this.categoryService.findOne(categoryId);
+				this.categoryService.delete(category);
+				this.categoryService.flush();
+				unauthenticate();
+			}catch (Throwable oops) {
+				caught = oops.getClass();
+			}
+			checkExceptions(expected, caught);
+			
+		}
 	
 }
