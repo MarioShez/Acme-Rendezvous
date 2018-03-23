f6387ab5f139ece1803e5de77c77d3bcb2c31eea
Juan Carlos Cansino <jucansu@gmail.com>
2018-03-23 22:09:30 +0100

    Revert "Aclaración en ManagerServiceTest.class"
    This reverts commit bccba7d2d566019629c10fa4d32210588eac58c1.

--- Workspace 2.0/Acme-Rendezvous-2.0/src/test/java/services/ManagerServiceTest.java
+++ Workspace 2.0/Acme-Rendezvous-2.0/src/test/java/services/ManagerServiceTest.java
@@ -1,4 +1,3 @@
-
 package services;
 
 import java.text.ParseException;
@@ -20,118 +19,124 @@ import utilities.AbstractTest;
 import domain.Manager;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = {
-	"classpath:spring/junit.xml"
-})
+@ContextConfiguration(locations={"classpath:spring/junit.xml"})
 @Transactional
-public class ManagerServiceTest extends AbstractTest {
-
+public class ManagerServiceTest extends AbstractTest{
+	
 	@Autowired
-	private ManagerService	managerService;
-
+	private ManagerService managerService;
+	
 	@PersistenceContext
-	private EntityManager	entityManager;
-
-
+	private EntityManager entityManager;
+	
+	
 	//Test Login---------------------------------------
-
-	@Test
-	public void loginDriver() {
-		final Object testingData[][] = {
-			{
-				//Positive Test: creando manager correctamente
-				"manager1", null
-			}, {
-				//Negative Test:creando manager sin name
-				"DonManuee", IllegalArgumentException.class
+	
+		@Test
+		public void loginDriver(){
+			final Object testingData[][] = {
+				{
+					//Positive Test: creando manager correctamente
+					"manager1",null
+				}
+				,{
+					//Negative Test:creando manager sin name
+					"DonManuee", IllegalArgumentException.class
+				}
+			};
+			
+			for (int i = 0; i<testingData.length; i++){
+				this.templateLoginManager((String) testingData[i][0], (Class<?>) testingData[i][1]);
 			}
-		};
-
-		for (int i = 0; i < testingData.length; i++)
-			this.templateLoginManager((String) testingData[i][0], (Class<?>) testingData[i][1]);
-	}
-
-	private void templateLoginManager(final String manager, final Class<?> expected) {
-		Class<?> caught;
-		caught = null;
-
-		try {
-			super.authenticate(manager);
-			this.unauthenticate();
-			this.managerService.flush();
-
-		} catch (final Throwable oops) {
-			caught = oops.getClass();
-			this.entityManager.clear();
 		}
-
-		this.checkExceptions(expected, caught);
-	}
-
-	//Acme-Rendezvous 2.0 3.1 Register to the system as a manager.
+		
+		private void templateLoginManager(final String manager, final Class<?> expected){
+			Class<?> caught;
+			caught = null;
+			
+			try{
+				super.authenticate(manager);
+				this.unauthenticate();
+				this.managerService.flush();
+				
+			}catch (final Throwable oops){
+				caught = oops.getClass();
+				this.entityManager.clear();
+			}
+			
+			this.checkExceptions(expected, caught);
+		}
+		
+		//3.1 Register to the system as a manager.
 	@Test
-	public void registrationManagerDriver() {
+	public void registrationManagerDriver(){
 		final Object testingData[][] = {
 			{
 				//Positive Test: creando manager correctamente
-				"manager1", "surname1", "1A-2B", "email1@gmail.com", "612345678", "Address1", "18/10/1993", "managerName1", "password", null
-			}, {
+				"manager1", "surname1", "1A-2B" ,"email1@gmail.com", "612345678", "Address1", "18/10/1993", "managerName1", "password", null
+			}
+			,{
 				//Negative Test: creando manager sin name
-				"", "surname1", "1A-2B", "email1@gmail.com", "612345678", "Address1", "18/10/1993", "managerName1", "password", DataIntegrityViolationException.class
-			}, {
+				"", "surname1","1A-2B" , "email1@gmail.com", "612345678", "Address1", "18/10/1993", "managerName1", "password", DataIntegrityViolationException.class
+			}
+			,{
 				//Negative Test: creando manager sin surname
-				"manager1", "", "1A-2B", "email1@gmail.com", "612345678", "Address1", "18/10/1993", "managerName1", "password", DataIntegrityViolationException.class
+				"manager1", "","1A-2B" , "email1@gmail.com", "612345678", "Address1", "18/10/1993", "managerName1", "password", DataIntegrityViolationException.class
 			}
-
-			, {
+			
+			,{
 				//Negative Test: creando manager sin email
-				"manager1", "surname1", "1A-2B", "", "612345678", "Address1", "18/10/1993", "managerName1", "password", DataIntegrityViolationException.class
+				"manager1", "surname1","1A-2B" , "", "612345678", "Address1", "18/10/1993", "managerName1", "password", DataIntegrityViolationException.class
 			}
-
-			, {
+			
+			,{
 				//Negative Test: creando manager sin teléfono
-				"manager1", "surname1", "1A-2B", "email1@gmail.com", "", "Address1", "18/10/1993", "managerName1", "password", DataIntegrityViolationException.class
+				"manager1", "surname1","1A-2B" , "email1@gmail.com", "", "Address1", "18/10/1993", "managerName1", "password", DataIntegrityViolationException.class
 			}
-
-			, {
+			
+			,{
 				//Negative Test: creando manager sin dirección
-				"manager1", "surname1", "1A-2B", "email1@gmail.com", "612345678", "", "18/10/1993", "managerName1", "password", DataIntegrityViolationException.class
-			}, {
+				"manager1", "surname1","1A-2B" , "email1@gmail.com", "612345678", "", "18/10/1993", "managerName1", "password", DataIntegrityViolationException.class
+			}
+			,{
 				//Negative Test: creando manager sin fecha de nacimiento
-				"manager1", "surname1", "1A-2B", "email1@gmail.com", "612345678", "Address1", "", "managerName1", "password", DataIntegrityViolationException.class
-			}, {
+				"manager1", "surname1","1A-2B" , "email1@gmail.com", "612345678", "Address1", "", "managerName1", "password", DataIntegrityViolationException.class
+			}
+			,{
 				//Negative Test: creando manager sin username
-				"manager1", "surname1", "1A-2B", "email1@gmail.com", "612345678", "Address1", "18/10/1993", "", "password", javax.validation.ConstraintViolationException.class
-			}, {
+				"manager1", "surname1","1A-2B" , "email1@gmail.com", "612345678", "Address1", "18/10/1993", "", "password", javax.validation.ConstraintViolationException.class
+			}
+			,{
 				//Negative Test: creando manager sin contraseña
-				"manager1", "surname1", "1A-2B", "email1@gmail.com", "612345678", "Address1", "18/10/1993", "managerName1", "", DataIntegrityViolationException.class
-			}, {
+				"manager1", "surname1","1A-2B" , "email1@gmail.com", "612345678", "Address1", "18/10/1993", "managerName1", "", DataIntegrityViolationException.class
+			}
+			,{
 				//Negative Test: creando manager sin VAT
-				"manager1", "surname1", "", "email1@gmail.com", "612345678", "Address1", "18/10/1993", "managerName1", "password", DataIntegrityViolationException.class
+				"manager1", "surname1", "" ,"email1@gmail.com", "612345678", "Address1", "18/10/1993", "managerName1", "password", DataIntegrityViolationException.class
 			}
 		};
-
-		for (int i = 0; i < testingData.length; i++)
-			this.templateCreateManager((String) testingData[i][0], (String) testingData[i][1], (String) testingData[i][2], (String) testingData[i][3], (String) testingData[i][4], (String) testingData[i][5], (String) testingData[i][6],
-				(String) testingData[i][7], (String) testingData[i][8], (Class<?>) testingData[i][9]);
+		
+		for (int i = 0; i<testingData.length; i++){
+			this.templateCreateManager((String) testingData[i][0], (String) testingData[i][1], (String) testingData[i][2], (String) testingData[i][3], (String) testingData[i][4], (String) testingData[i][5], (String) testingData[i][6], (String) testingData[i][7], (String) testingData[i][8], (Class<?>) testingData[i][9]);
+		}
 	}
-
-	private void templateCreateManager(final String name, final String surname, final String vat, final String email, final String phone, final String address, final String birth, final String username, final String password, final Class<?> expected) {
+	
+	private void templateCreateManager(final String name, final String surname,final String vat, final String email, final String phone, final String address, final String birth, final String username, final String password, final Class<?> expected){
 		Class<?> caught;
 		Manager manager;
 		caught = null;
 		Date fecha = new Date();
-
-		final SimpleDateFormat pattern = new SimpleDateFormat("dd/MM/yyyy");
-		try {
+		
+		SimpleDateFormat pattern = new SimpleDateFormat("dd/MM/yyyy");
+		try{
 			fecha = pattern.parse(birth);
-		} catch (final ParseException e) {
+		}catch(ParseException e){
 			e.getClass();
 		}
-
-		try {
+		
+		try{
 			manager = this.managerService.create();
-
+			
 			manager.setName(name);
 			manager.setSurname(surname);
 			manager.setEmail(email);
@@ -141,52 +146,54 @@ public class ManagerServiceTest extends AbstractTest {
 			manager.getUserAccount().setUsername(username);
 			manager.getUserAccount().setPassword(password);
 			manager.setVat(vat);
-
+			
+			
 			manager = this.managerService.save(manager);
-
+			
 			this.managerService.flush();
-		} catch (final Throwable oops) {
+		}catch (final Throwable oops){
 			caught = oops.getClass();
 			this.entityManager.clear();
 		}
-
+		
 		this.checkExceptions(expected, caught);
 	}
 
 	@Test
-	public void editManagerDriver() {
+	public void editManagerDriver(){
 		final Object testingData[][] = {
 			{
 				//Positive Test: creando manager correctamente
-				"manager1", "surname1", "1A-2B", "email1@gmail.com", "612345678", "Address1", "18/10/1993", null
-			}, {
+				"manager1", "surname1", "1A-2B" ,"email1@gmail.com", "612345678", "Address1", "18/10/1993",  null
+			}
+			,{
 				//Negative Test: creando manager sin name
-				"", "surname1", "1A-2B", "email1@gmail.com", "612345678", "Address1", "18/10/1993", javax.validation.ConstraintViolationException.class
+				"", "surname1","1A-2B" , "email1@gmail.com", "612345678", "Address1", "18/10/1993",  javax.validation.ConstraintViolationException.class
 			}
 
 		};
-
-		for (int i = 0; i < testingData.length; i++)
-			this.templateEditManager((String) testingData[i][0], (String) testingData[i][1], (String) testingData[i][2], (String) testingData[i][3], (String) testingData[i][4], (String) testingData[i][5], (String) testingData[i][6],
-				(Class<?>) testingData[i][7]);
+		
+		for (int i = 0; i<testingData.length; i++){
+			this.templateEditManager((String) testingData[i][0], (String) testingData[i][1], (String) testingData[i][2], (String) testingData[i][3], (String) testingData[i][4], (String) testingData[i][5], (String) testingData[i][6],  (Class<?>) testingData[i][7]);
+		}
 	}
-
-	private void templateEditManager(final String name, final String surname, final String vat, final String email, final String phone, final String address, final String birth, final Class<?> expected) {
+	
+	private void templateEditManager(final String name, final String surname,final String vat, final String email, final String phone, final String address, final String birth,  final Class<?> expected){
 		Class<?> caught;
 		Manager manager;
 		caught = null;
 		Date fecha = new Date();
-
-		final SimpleDateFormat pattern = new SimpleDateFormat("dd/MM/yyyy");
-		try {
+		
+		SimpleDateFormat pattern = new SimpleDateFormat("dd/MM/yyyy");
+		try{
 			fecha = pattern.parse(birth);
-		} catch (final ParseException e) {
+		}catch(ParseException e){
 			e.getClass();
 		}
-
-		try {
+		
+		try{
 			manager = this.managerService.create();
-
+			
 			manager.setName(name);
 			manager.setSurname(surname);
 			manager.setEmail(email);
@@ -194,16 +201,17 @@ public class ManagerServiceTest extends AbstractTest {
 			manager.setAddress(address);
 			manager.setBirth(fecha);
 			manager.setVat(vat);
-
+			
+			
 			manager = this.managerService.save(manager);
-
+			
 			this.managerService.flush();
-		} catch (final Throwable oops) {
+		}catch (final Throwable oops){
 			caught = oops.getClass();
 			this.entityManager.clear();
 		}
-
+		
 		this.checkExceptions(expected, caught);
 	}
-
+	
 }
